c32266e0ae0902cbb158894a2919dab375b8101e
johann219 <147496124+johann219@users.noreply.github.com>
2025-05-01 11:53:10 +0300

    Refactor spell object constructor to class

--- modules/spell.js
+++ modules/spell.js
@@ -1,53 +1,48 @@
-const SpellStats = function (
-    spellRange = null,
-    spellArea = null,
-    spellTarget = null,
-    spellSaving = null,
-    spellDuration = null) {
-        if(!new.target) {
-            throw Error ("Need 'new' keyword to call a constructor");
-        }
-
-        this.spellRange = spellRange;
-        this.spellArea = spellArea;
-        this.spellTarget = spellTarget;
-        this.spellSaving = spellSaving;
-        this.spellDuration = spellDuration;
+const SpellStats = class {
+    constructor(
+        spellRange = null,
+        spellArea = null,
+        spellTarget = null,
+        spellSaving = null,
+        spellDuration = null) {
+            this.spellRange = spellRange;
+            this.spellArea = spellArea;
+            this.spellTarget = spellTarget;
+            this.spellSaving = spellSaving;
+            this.spellDuration = spellDuration;
+    }
 }
 
-const Spell = function (
-    spellName, 
-    spellType,
-    spellLevel,
-    spellTraits,
-    spellTraditions,
-    spellCast,
-    spellStats,
-    spellDescription,
-    spellHeightened,
-    spellKnown = false) {
-        if(!new.target) {
-            throw Error ("Need 'new' keyword to call a constructor");
-        }
-
-        this.spellName = spellName;
-        this.spellType = spellType;
-        this.spellLevel = spellLevel;
-        this.spellTraits = spellTraits.slice();
-        this.spellTraditions = spellTraditions.slice();
-        this.spellCast = spellCast.slice();
+const Spell = class {
+    constructor(
+        spellName, 
+        spellType,
+        spellLevel,
+        spellTraits,
+        spellTraditions,
+        spellCast,
+        spellStats,
+        spellDescription,
+        spellHeightened,
+        spellKnown = false) {
+            this.spellName = spellName;
+            this.spellType = spellType;
+            this.spellLevel = spellLevel;
+            this.spellTraits = spellTraits.slice();
+            this.spellTraditions = spellTraditions.slice();
+            this.spellCast = spellCast.slice();
         
-        this.spellStats = new SpellStats(
-            spellStats.spellRange,
-            spellStats.spellArea,
-            spellStats.spellTarget,
-            spellStats.spellSaving,
-            spellStats.spellDuration,
-        );
-        
-        this.spellDescription = spellDescription;
-        this.spellHeightened = spellHeightened;
-        this.spellKnown = spellKnown;
+            this.spellStats = new SpellStats(
+                spellStats.spellRange,
+                spellStats.spellArea,
+                spellStats.spellTarget,
+                spellStats.spellSaving,
+                spellStats.spellDuration,
+            );
+            this.spellDescription = spellDescription;
+            this.spellHeightened = spellHeightened;
+            this.spellKnown = spellKnown;
+    }
 }
 
 export { Spell };
